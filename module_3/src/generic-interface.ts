f0b30ea31a474e5530a496bfaf26a912a72dd664
tawsifhye <tawsif.lucse@gmail.com>
2023-05-06 01:43:42 +0600

    Generic in Interface

--- module_3/src/generic-interface.ts
+++ module_3/src/generic-interface.ts
@@ -16,7 +16,12 @@ interface TestInterface2<T, U=null>{
     name: 'Tawsif',
     id:1
   }
-  const interfaceObject2:TestInterface2<number>={
+  const interfaceObject3:TestInterface2<number, {holding:string, postalCode:string}>={
     name: 'Tawsif',
-    id:1
+    id:1,
+    address:{
+      holding: "29 Agrani",
+      postalCode:"Sylhet 3100"
+    }
   }
+ 
